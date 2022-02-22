c23b4e9eb90175b0e6de995af1d271cd4550327a
Dilara Tekin <94602968+dilaratekkinn@users.noreply.github.com>
2022-02-22 15:09:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const buttonSelector = document.getElementById('button');
+const buttonCtaSelector = document.getElementById('cta');
+const loaderSelector = document.getElementById('loader');
+const jokeSelector = document.getElementById('joke');
+const errorContainerSelector = document.getElementById('error-container');
+const errorSelector = document.getElementById('error-message');
+
+const API_ENDPOINT = 'https://icanhazdadjoke.com/';
+const XHR = new XMLHttpRequest();
+
+function setDisabledUiState(isDisabled) {
+    setLoaderState(isDisabled);
+    setButtonState(isDisabled);
+}
+
+function showJoke(joke) {
+    setDisabledUiState(false)
+    jokeSelector.innerHTML = joke;
+}
+
+function showError(error) {
+    setDisabledUiState(false)
+    errorSelector.innerHTML = error;
+    errorContainerSelector.style.display = 'block';
+}
+
+function setLoaderState(isVisible) {
+    const displayState = isVisible ? 'block' : 'none';
+    loaderSelector.style.display = displayState;
+}
+
+function setButtonState(isDisabled) {
+    if (isDisabled) {
+        buttonSelector.setAttribute('disabled', 'disabled');
+    } else {
+        buttonSelector.removeAttribute('disabled');
+    }
+
+    const buttonCtaState = isDisabled ? 'none' : 'block';
+    buttonCtaSelector.style.display = buttonCtaState;
+}
+
+function setButtonCta(isError) {
+    const buttonCta = isError ? 'Try again' : 'Get another one';
+    buttonCtaSelector.innerHTML = buttonCta;
+}
+
+function getJoke() {
+    XHR.open('GET', API_ENDPOINT);
+
+    XHR.setRequestHeader('Accept', 'application/json');
+    XHR.responseType = 'json';
+
+    XHR.onload = function() {
+        showJoke(XHR.response.joke);
+        setButtonCta(false);
+    }
+
+    XHR.onerror = function() {
+        showError('An error occurred, please try again');
+        setButtonCta(true);
+    }
+
+    XHR.send();
+}
+
+buttonSelector.addEventListener('click', function() {
+    setDisabledUiState(true)
+    getJoke();
+});
